f2fe6a9ad48052cf7d014fb8b1551b6997f9603a
Abdessamad-elhallaoui <abdessamad.elhallaoui@outlook.fr>
2020-01-19 22:06:25 +0100

    new

---
+++ breif4/home.js
@@ -0,0 +1,7 @@
+var prv = document.getElementById("prevBtn");
+var nxt =document.getElementById("nextBtn");
+var slides=document.getElementsByClassName("slide1");
+var index=1;
+
+var i =slides.length;
+console.log(i);
